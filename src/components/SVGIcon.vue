a78412fcfec60abcd86fa9494e05ccfe10aeb95c
Marcus Ortiz <marcus_ortiz@apple.com>
2022-05-19 17:11:03 -0700

    Use xlink:href.

--- src/components/SVGIcon.vue
+++ src/components/SVGIcon.vue
@@ -9,13 +9,14 @@
 -->
 
 <template>
-  <img v-if="themeOverrideURL" :src="themeOverrideURL" />
-  <svg v-else
+  <svg
     aria-hidden="true"
     class="svg-icon"
     xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
   >
-    <slot />
+    <use v-if="themeOverrideURL" :href="`${themeOverrideURL}#${themeId}`" />
+    <slot v-else />
   </svg>
 </template>
 
