3a0dfa999fa15f46b76f1066c86a269311a5f5b7
Marcus Ortiz <marcus_ortiz@apple.com>
2022-06-30 13:23:24 -0700

    Fix bug where theme colors only worked with Auto scheme.

--- src/App.vue
+++ src/App.vue
@@ -63,8 +63,19 @@ export default {
   computed: {
     currentColorScheme: ({ appState }) => appState.systemColorScheme,
     preferredColorScheme: ({ appState }) => appState.preferredColorScheme,
-    CSSCustomProperties: ({ themeSettings, currentColorScheme }) => (
-      objectToCustomProperties(themeSettings.theme, currentColorScheme)
+    CSSCustomProperties: ({
+      currentColorScheme,
+      preferredColorScheme,
+      themeSettings,
+    }) => (
+      // If the user has selected "Auto", delegate to the system's current
+      // preference to determine if "Light" or "Dark" colors should be used.
+      // Otherwise, if "Light" or "Dark" has been explicitly chosen, that choice
+      // should be used directly.
+      objectToCustomProperties(themeSettings.theme, (preferredColorScheme === ColorScheme.auto.value
+        ? currentColorScheme
+        : preferredColorScheme
+      ))
     ),
     hasCustomHeader: () => !!window.customElements.get('custom-header'),
     hasCustomFooter: () => !!window.customElements.get('custom-footer'),
